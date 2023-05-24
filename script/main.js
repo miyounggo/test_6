acc28502fb16a9f3aad0bed3de584f0392da4901
okemos <gmy68@hanmail.net>
2023-05-24 12:15:45 +0900

    changes

---
+++ script/main.js
@@ -0,0 +1,19 @@
+document.querySelector("#btn01").addEventListener("click", () => {
+    alert("무등산 상세페이지로 이동합니다!");
+    location.href=http://xn--q20b22d63gstp7hax35e.com/
+});
+document.querySelector("#btn01").addEventListener("click", () => {
+    alert("무등산 상세페이지로 이동합니다!");
+});
+document.querySelector("#btn01").addEventListener("click", () => {
+    alert("무등산 상세페이지로 이동합니다!");
+});
+document.querySelector("#btn01").addEventListener("click", () => {
+    alert("무등산 상세페이지로 이동합니다!");
+});
+document.querySelector("#btn01").addEventListener("click", () => {
+    alert("무등산 상세페이지로 이동합니다!");
+});
+document.querySelector("#btn01").addEventListener("click", () => {
+    alert("무등산 상세페이지로 이동합니다!");
+});
